c5f98a18ae4245afe7d30abcba6f038db982382b
John Hagler <john.hagler@treetop.com>
2016-07-20 09:43:26 -0700

    Update init.sql

--- import/init.sql
+++ import/init.sql
@@ -11,14 +11,29 @@ CREATE TABLE patients (
 CREATE INDEX patients_PK ON patients (patient_id);
 
 
+DROP TABLE practices IF EXISTS;
+
+CREATE TABLE practices (
+  practice_id integer PRIMARY KEY UNIQUE,
+  name text,
+  location text
+);
+
+CREATE INDEX practices_PK ON practices (practice_id);
+
+
 DROP TABLE encounters IF EXISTS;
 
 CREATE TABLE encounters (
   encounter_id integer PRIMARY KEY UNIQUE,
   patient_id integer,
+  practice_id integer,
   timestamp text,
-  FOREIGN KEY(patient_id) REFERENCES patients(patient_id)
+  FOREIGN KEY(patient_id) REFERENCES patients(patient_id),
+  FOREIGN KEY(practice_id) REFERENCES practices(practice_id)
 );
 
 CREATE INDEX encounters_PK ON patients (encounter_id);
+CREATE INDEX encounters_FK_patient_id ON patients (patient_id);
+CREATE INDEX encounters_FK_practice_id ON patients (practice_id);
 
