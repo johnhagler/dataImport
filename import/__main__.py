a833b639ce20a81102b99e3f21e9567b610c0acb
John Hagler <haglerjo@gmail.com>
2016-07-19 18:47:37 -0700

    handle if db path does not exist

--- import/__main__.py
+++ import/__main__.py
@@ -7,16 +7,19 @@ def main():
 
     db_path = sys.argv[1]
 
-    if not os.path.exists(db_path):
+    if not os.path.exists(os.path.dirname(db_path)):
         abs_path = os.path.abspath(db_path)
-        os.mkdir(os.path.dirname(abs_path))
+        print '>>> Attempting to create new database: ' + abs_path
+        print '>>> Folder %s does not exist' % os.path.dirname(abs_path)
+        print '>>> Please create it.'
+        sys.exit(1)
 
     process = sys.argv[2]
 
     if process == 'cvx':
         cvx_codes.main(db_path)
     else:
-        print 'no options'
+        print 'choose which data to import  [cvx]'
 
 
 if __name__ == '__main__':
